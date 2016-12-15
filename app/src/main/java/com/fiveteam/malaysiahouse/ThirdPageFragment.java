78987583cb7aa5c07c64bde40fbb8b09efc470d5
cibocoda <zopulee@gmail.com>
2016-12-15 21:57:09 +0800

    初始化

---
+++ app/src/main/java/com/fiveteam/malaysiahouse/ThirdPageFragment.java
@@ -0,0 +1,39 @@
+package com.fiveteam.malaysiahouse;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThirdPageFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public ThirdPageFragment() {
+        // Required empty public constructor
+    }
+
+    public static ThirdPageFragment newInstance(int sectionNumber) {
+        ThirdPageFragment fragment = new ThirdPageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_third_page, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
+}
