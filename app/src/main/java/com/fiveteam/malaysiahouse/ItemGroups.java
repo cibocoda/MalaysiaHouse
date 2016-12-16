d22577b4cda50bcacb482d26139150291589203d
cibocoda <zopulee@gmail.com>
2016-12-16 16:39:06 +0800

    page3 list完成

--- app/src/main/java/com/fiveteam/malaysiahouse/ItemGroups.java
+++ app/src/main/java/com/fiveteam/malaysiahouse/ItemGroups.java
@@ -8,6 +8,10 @@ public class ItemGroups {
     int tabIcon[]={ R.drawable.icon_home, R.drawable.ic_search_black_24dp,
             R.drawable.ic_tag_faces_black_24dp, R.drawable.ic_apps_black_24dp};
 
+    int page3_list_icons[]={ R.drawable.ic_star_black_24dp, R.drawable.ic_access_time_black_24dp,
+            R.drawable.ic_phone_black_24dp, R.drawable.ic_chat_black_24dp, R.drawable.ic_forum_black_24dp,
+            R.drawable.ic_note_add_black_24dp, R.drawable.ic_favorite_black_24dp };
+
     int page4_list_icons[]={ R.drawable.ic_event_note_black_24dp, R.drawable.ic_thumb_up_black_24dp,
             R.drawable.ic_create_black_24dp, R.drawable.ic_send_black_24dp,
             R.drawable.ic_phonelink_setup_black_24dp, R.drawable.ic_info_outline_black_24dp};
--- app/src/main/java/com/fiveteam/malaysiahouse/ThirdPageFragment.java
+++ app/src/main/java/com/fiveteam/malaysiahouse/ThirdPageFragment.java
@@ -9,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -17,6 +20,9 @@ import android.widget.TextView;
  */
 public class ThirdPageFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private int imageId[]=new ItemGroups().page3_list_icons;
+    private String p3Listitems[];
+    private ListView mListView;
 
     public ThirdPageFragment() {
         // Required empty public constructor
@@ -35,8 +41,6 @@ public class ThirdPageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_third_page, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
 
         Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
@@ -44,8 +48,53 @@ public class ThirdPageFragment extends Fragment {
         final ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
         ab.setDisplayShowCustomEnabled(true);
         ab.setDisplayShowTitleEnabled(false);
+        p3Listitems=getResources().getStringArray(R.array.page3_main_list);
+        mListView = (ListView) rootView.findViewById(R.id.LV_p3);
+        mListView.setAdapter(new p3ListAdapter());
 
         return rootView;
+    }
+
+    private class p3ListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return p3Listitems.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            Holder holder;
+            if(v == null){
+                v = LayoutInflater.from(getActivity()).inflate(R.layout.list_item, null);
+                holder = new Holder();
+                holder.image = (ImageView) v.findViewById(R.id.IV_listItem);
+                holder.text = (TextView) v.findViewById(R.id.TV_listItem);
+
+                v.setTag(holder);
+            } else{
+                holder = (Holder) v.getTag();
+            }
+
+            holder.image.setImageResource(imageId[position]);
+            holder.text.setText(p3Listitems[position]);
+            return v;
+        }
+        class Holder{
+            ImageView image;
+            TextView text;
+        }
     }
 
 }
